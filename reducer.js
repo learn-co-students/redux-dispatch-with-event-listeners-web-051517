c957c8a42f4f73f6da21a454bf62e864662fb241
sochetanie <allagorik2901@gmail.com>
2017-08-02 22:11:49 -0400

    done

--- reducer.js
+++ reducer.js
@@ -1,9 +1,33 @@
-let state;
-function changeState(state = {count: 0}, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+let state
+ 
+function changeState(state = { count: 0 }, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 }
+    case 'DECREASE_COUNT':
+      return { count: state.count - 1 }
+    default:
+      return state
   }
+}
+
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+
+function render(){
+  let container = document.getElementById('container')
+  container.textContent = state.count
+}
+
+dispatch({ type: '@@INIT' })
+
+
+  document.getElementById('buttonI').addEventListener('click', function(){
+    dispatch({type: 'INCREASE_COUNT'})
+  })
+
+  document.getElementById('buttonD').addEventListener('click', function(){
+    dispatch({type: 'DECREASE_COUNT'})
+  })
